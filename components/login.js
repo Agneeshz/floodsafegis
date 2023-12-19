23836e1f118536f680d8b4e281858fb31147e093
Biju Borah <biju20_ug@ee.nits.ac.in>
2023-12-19 19:12:54 +0530

    added loader

--- components/login.js
+++ components/login.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { setCookie } from 'nookies';
 import { BASE_API_URL } from "@/utils/constants";
+import Loader from "./Loader";
 
 const LoginComp = ({ type }) => {
   const router = useRouter();
@@ -16,12 +17,15 @@ const LoginComp = ({ type }) => {
     password: "",
   });
 
+  const [loading, setLoading] = useState(false);
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = async (type) => {
+    setLoading(true);
     try {
       if (type === 'register') {
         const response = await axios.post(`https://hydro-predict.onrender.com/auth/register/`, formData);
@@ -54,80 +58,86 @@ const LoginComp = ({ type }) => {
     } catch (error) {
       console.error("Something went wrong", error);
     }
+    setLoading(false);
   };
 
   return (
-    <div className={styles.loginContainer}>
-      {type === "login" ? <h2> Welcome Back!</h2> : <h2>Create Account</h2>}
-      <div className={styles.inputList}>
-        {type === "register" && (
-          <>
+    <>
+      {loading ? <Loader /> :
+        < div className={styles.loginContainer} >
+          {type === "login" ? <h2> Welcome Back!</h2> : <h2>Create Account</h2>
+          }
+          <div className={styles.inputList}>
+            {type === "register" && (
+              <>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  name="first_name"
+                  value={formData.first_name}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+              </>
+            )}
             <input
               type="text"
-              placeholder="Enter your name"
-              name="first_name"
-              value={formData.first_name}
+              placeholder="Enter a username"
+              name="username"
+              value={formData.username}
               onChange={handleInputChange}
             />
             <input
-              type="email"
-              placeholder="Enter your email"
-              name="email"
-              value={formData.email}
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              value={formData.password}
               onChange={handleInputChange}
             />
-          </>
-        )}
-        <input
-          type="text"
-          placeholder="Enter a username"
-          name="username"
-          value={formData.username}
-          onChange={handleInputChange}
-        />
-        <input
-          type="password"
-          placeholder="Enter your password"
-          name="password"
-          value={formData.password}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className={styles.checkboxAndForgetPass}>
-        <div className={styles.checkbox}>
-          <input type="checkbox" name="remember" id="remember" />
-          <label htmlFor="remember">Remember Me</label>
-        </div>
-        {type === "login" && (
-          <div
-            className={styles.forgotPass}
-            onClick={() => router.push("/forgotPassword")}
-          >
-            Forgot Password?
           </div>
-        )}
-      </div>
-      <div className={styles.continueBtn} onClick={() => (handleSubmit(type))}>
-        <Button text={"Continue"} alignment="center" />
-      </div>
-      <div className={styles.switchDialog}>
-        {type === "login" ? (
-          <p>
-            Don&apos;t have an account?{" "}
-            <span>
-              <Link href={"/register"}>Sign Up</Link>
-            </span>
-          </p>
-        ) : (
-          <p>
-            Already have an account?{" "}
-            <span>
-              <Link href={"/login"}>Log In</Link>
-            </span>
-          </p>
-        )}
-      </div>
-    </div>
+          <div className={styles.checkboxAndForgetPass}>
+            <div className={styles.checkbox}>
+              <input type="checkbox" name="remember" id="remember" />
+              <label htmlFor="remember">Remember Me</label>
+            </div>
+            {type === "login" && (
+              <div
+                className={styles.forgotPass}
+                onClick={() => router.push("/forgotPassword")}
+              >
+                Forgot Password?
+              </div>
+            )}
+          </div>
+          <div className={styles.continueBtn} onClick={() => (handleSubmit(type))}>
+            <Button text={"Continue"} alignment="center" />
+          </div>
+          <div className={styles.switchDialog}>
+            {type === "login" ? (
+              <p>
+                Don&apos;t have an account?{" "}
+                <span>
+                  <Link href={"/register"}>Sign Up</Link>
+                </span>
+              </p>
+            ) : (
+              <p>
+                Already have an account?{" "}
+                <span>
+                  <Link href={"/login"}>Log In</Link>
+                </span>
+              </p>
+            )}
+          </div>
+        </div >
+      }
+    </>
   );
 };
 
