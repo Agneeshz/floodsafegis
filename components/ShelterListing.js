2de4afc589ed8a553795f897588c8b71710a8f90
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2023-12-20 15:19:01 +0530

    done

--- components/ShelterListing.js
+++ components/ShelterListing.js
@@ -21,11 +21,21 @@ const ShelterListing = () => {
 
     fetchData();
   }, []);
-  const handleDelete = () => {
-    // Add your delete logic here
-    console.log('Item deleted');
-  };
-
+  
+  // const handleDelete = async (shelterId) => {
+  //   try {
+  //     await axios.delete(`https://hydro-predict.onrender.com/api/shelters/${shelterId}`);
+  
+  //     setShelters((prevShelters) => prevShelters.filter((shelter) => shelter._id !== shelterId));
+  //   } catch (error) {
+  //     console.error('Error deleting shelter:', error);
+  //   }
+  // };  
+  
+  const filteredShelters = shelters.filter(shelter =>
+    shelter.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+  );
+  
   return (
     <div className={styles.shelterListing}>
       <div className={styles.searchinputcontainer}>
@@ -36,26 +46,28 @@ const ShelterListing = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
       </div>
-      <div>  
+      <div style={{ marginBottom:"2vh", border:"1px solid gray"}}>  
         <div className={styles.gridContainer}>
         <div>Location</div>
         <div>Latitude</div>
         <div>Longitude</div>  
         <div>Description</div>
-        <div></div>
+        
         </div>
-        <div>
-        {shelters.map((shelter,idx) => (
+        <div className={styles.listingsGrid} style={{height:"46vh",overflow:"auto",}}>
+        {filteredShelters.map((shelter,idx) => (
           <div key={idx} className={styles.contentGrid}>
           <div>{shelter.name}</div> 
           <div>{parseFloat(shelter.lat).toFixed(4)}</div>
           <div>{parseFloat(shelter.lng).toFixed(4)}</div>  
-          <div>read more</div>
-          <div style={{display:"flex",alignItems:"center",}}>          <button className={styles.button} onClick={() => handleEditClick(shelter.id)}>Edit</button>
-            {/* <p>Identifier: {shelter.identifier.username}</p> */}
-            <button className={styles.trashCan} onClick={handleDelete}>
+          <div>{shelter.description}</div>
+          {/* <div style={{display:"flex",alignItems:"center", gap:"2vw"}}>           */}
+          {/* <button className={styles.button} onClick={() => handleEditClick(shelter._id)}>Edit</button> */}
+            {/* <p>Identifier: {shelter.identifier.username}</p>
+            <button className={styles.trashCan} onClick={() => handleDelete(shelter._id)}>
             <FontAwesomeIcon icon={faTrashCan} />
-            </button></div>
+            </button> */}
+            {/* </div> */}
 
           </div>
         ))}
